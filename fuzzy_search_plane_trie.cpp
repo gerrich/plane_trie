59bfdf3494c7041b76e7aac328e25a02161bc823
Georgy Ivanov <ivanov.georg@gmail.com>
2013-07-16 19:52:12 +0400

    add fuzzy search & fix comparator

---
+++ fuzzy_search_plane_trie.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "plane_trie.hpp"
+
+#include "mmap.hpp"
+
+int main(int argc, char**argv) {
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <trie.bin>" << std::endl;
+    return 1;
+  }
+
+  mmap_t my_map(argv[1]);
+ 
+  trie_t trie;
+  load_trie(my_map.data(), my_map.size(), trie);
+
+  size_t max_dist = 3;
+
+  while(std::cin.good()) {
+    std::string line;
+    getline(std::cin, line);
+    if (line.empty()) continue;
+
+    fuzzy_search(trie, line.c_str(), max_dist);
+    //std::cout << line << "\t" << value << std::endl;
+  }
+
+  return 0;
+}
